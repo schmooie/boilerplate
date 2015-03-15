9d502d84c401852472a185a39ad21161b78b4d4e
Sam Lau <samuel.kwun.lau@gmail.com>
2015-03-15 19:16:06 -0400

    Created boilerplate.

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var server = express();
+var itemJSON = require('./item.json');
+
+var port = process.env.PORT || 3333;
+
+server.engine('html', require('ejs').renderFile);
+server.set('view engine', 'html');
+server.use(express.static(__dirname + '/public'));
+
+// api
+server.get('/item', function(req, res) {
+  res.json(itemJSON);
+});
+server.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+server.listen(port, function() {
+  console.log('Listening on port %d', port);
+});
